5ed1977e4675167d4435a153bc98980f92db05c6
k12311 <154042981+k12311@users.noreply.github.com>
2025-04-22 09:41:10 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('brothers-cache').then(cache => {
+      return cache.addAll(['./', './index.html', './icon-192.png', './icon-512.png']);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
